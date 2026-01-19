4c9d2a86bb890a90d273d3af6460124867be78df
Kevin Wang <wy721@qq.com>
2026-01-19 01:40:52 +0000

    Remove sdk from this repo

--- no_std_check/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-
-// SPDX-FileCopyrightText: © 2025 Phala Network <dstack@phala.network>
-//
-// SPDX-License-Identifier: BUSL-1.1
-
-// Import the types we want to test
-use dstack_sdk_types as _;
